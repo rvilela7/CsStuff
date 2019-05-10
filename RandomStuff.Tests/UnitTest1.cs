e30de14d06cfa761ee1f44927fd61d9f6edd269d
RuiVilela <ruivil@gmail.com>
2019-05-10 16:33:26 +0100

    Test Fix

--- RandomStuff.Tests/UnitTest1.cs
+++ RandomStuff.Tests/UnitTest1.cs
@@ -9,7 +9,9 @@ namespace RandomStuff.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.isFail(1==0);
+            var str = MD5Gen.CreateMD5("ABCDEF");
+            var result = "8827a41122a5028b9808c7bf84b9fcf6".ToUpper();
+            Assert.IsTrue(str.Equals(result));
         }
     }
 }
