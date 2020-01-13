48792abf49e62339b9fb0558c989373b97baf518
RuiVilela <ruivil@gmail.com>
2020-01-13 17:30:08 +0000

    Refact

--- Async/Classes/MyTask.cs
+++ Async/Classes/MyTask.cs
@@ -5,17 +5,6 @@ namespace Async.Classes
 {
     public class MyTask
     {
-        public void MySleep() => Thread.Sleep(4000);
-        public async Task MySleep2() => await Task.Delay(4000);
-
-        public async Task<string> MyTest1() { 
-            await MySleep2();
-            await MySleep2();
-            await MySleep2();
-            //MySleep();
-
-            Task.WaitAll();
-            return "reply";        
-        }
+        
     }
 }
--- Async/Program.cs
+++ Async/Program.cs
@@ -1,20 +1,108 @@
 ﻿using System;
-using Async.Classes;
+using System.Threading.Tasks;
+using System.IO;
 
-namespace Async
+namespace task_demo
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Start");
-            Console.ReadKey();
+  class Program
+  {
+      static async Task<string> ReadTxtFile() 
+      {
+          using(var sr = new StreamReader(File.Open("test.txt", FileMode.Open)))
+          {
+              return await sr.ReadToEndAsync();
+          }
+      }
 
-            MyTask tt = new MyTask();
-            var c = tt.MyTest1();
-        
-            Console.WriteLine("End");
+      static string ReadFileSync1() 
+      {
+          Task.Delay(2000).Wait();
+          return "content1";
+      }
 
-        }
-    }
+      static string ReadFileSync2()
+      {
+          Task.Delay(2000).Wait();
+          return "content2";
+      }
+
+      static string ReadFileSync3()
+          {
+          Task.Delay(2000).Wait();
+          return "content3";
+      }
+      
+      static async Task DoSomething()
+      {
+          await Task.Delay(2000);
+      }
+      static async Task<int> Sum(int a, int b) 
+      {
+          var result = await Task.FromResult(a + b);
+          return result;
+      }
+
+      static async Task<string> ReadFile1() 
+      {
+          await Task.Delay(3000);
+          return "file1";
+      }
+
+      static async Task<string> ReadFile2()
+      {
+          await Task.Delay(4000);
+          return "file2";
+      }
+
+      static async Task<string> ReadFile3()
+      {
+          await Task.Delay(2000);
+          return "file3";
+      }
+
+      static void Main(string[] args)
+      {
+          var start = DateTime.Now;
+          var c1 = ReadFileSync1();
+          var c2 = ReadFileSync2();
+          var c3 = ReadFileSync3();
+          var end = DateTime.Now;
+          Console.WriteLine("Time taken {0}", end-start);
+
+          start  = DateTime.Now;
+
+          var taskSum = Sum(2,2);
+          var taskDelay = DoSomething();
+
+          Task.WaitAll(taskSum, taskDelay);
+
+          end = DateTime.Now;
+
+          Console.WriteLine("{0}",taskSum.Result);
+
+          Console.WriteLine("Time taken! {0}", end-start);
+
+          var task1 = ReadFile1();
+          var task2 = ReadFile2();
+          var task3 = ReadFile3();
+
+          start = DateTime.Now;
+          Task.WaitAny(task1, task2, task3);
+          
+          
+          Console.WriteLine("Task1, completed: {0}", task1.IsCompleted);
+          
+          // this forces everyone to wait for this Task1
+          // Console.WriteLine("Task1, completed: {0}", task1.Result);
+
+          Console.WriteLine("Task2, completed: {0}", task2.IsCompleted);
+
+          Console.WriteLine("Task3, completed: {0}", task3.IsCompleted);
+          Console.WriteLine("Task3, completed: {0}", task3.Result);
+
+          end = DateTime.Now;
+          Console.WriteLine("Time taken! {0}", end - start);
+
+      }
+  }
 }
