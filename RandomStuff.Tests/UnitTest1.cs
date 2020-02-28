32153dcbd16cc52cae4f6ddc4465a64e4ff55090
Rui Vilela <ruivil@gmail.com>
2020-02-28 22:09:19 +0000

    Test2

--- RandomStuff.Tests/UnitTest1.cs
+++ RandomStuff.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using RandomStuff;
+using RandomStuff.MD5;
 
 namespace RandomStuff.Tests
 {
@@ -10,9 +12,20 @@ namespace RandomStuff.Tests
         }
 
         [Test]
-        public void Test1()
+        public void SimpleTest()
         {
             Assert.Pass();
         }
+
+        [Test]
+        public void MD5Test()
+        {
+            MD5Gen md5Gen = new MD5Gen();
+            Assert.IsNotNull(md5Gen);
+            string result = md5Gen.GetMD5("ABC");
+            Assert.IsNotEmpty(result);
+            Assert.AreEqual("902FBDD2B1DF0C4F70B4A5D23525E932", result);
+        }
+
     }
 }
