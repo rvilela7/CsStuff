5302cc4a35cb8d30da0bcadad5e95db067dcf2a8
RuiVilela <ruivil@gmail.com>
2020-09-18 15:27:23 +0100

    Small update

--- RandomStuff/MD5.cs
+++ RandomStuff/MD5.cs
@@ -1,27 +1,27 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using RandomStuff.MD5;
 
 namespace RandomStuff
 {
-    using RandomStuff.MD5;
-    class Application
+    class RandomNumbersMD5
     {
-        private readonly IMD5Gen _md5Gen;
-        public Application(IMD5Gen mD5Gen)
+        private readonly MD5Gen _md5Gen;
+        public RandomNumbersMD5(MD5Gen mD5Gen)
         {
             _md5Gen = mD5Gen;
         }
         public void run()
         {
             Random r = new Random();
-            var myLista = Enumerable.Range(0, 10)
+            var Numbers = Enumerable.Range(0, 10)
                                     .Select(i => r.Next(100))
                                     .ToList();
-            myLista.ForEach( m => Console.WriteLine(m));
+            Numbers.ForEach( m => Console.WriteLine(m));
             Console.WriteLine();
             List<KeyValuePair<int, string>> myMD5Res = new List<KeyValuePair<int, string>>();
-            myLista.ForEach(m => myMD5Res.Add( new KeyValuePair<int, string>(m, _md5Gen.GetMD5(m))));
+            Numbers.ForEach(m => myMD5Res.Add( new KeyValuePair<int, string>(m, _md5Gen.GetMD5(m))));
             myMD5Res.ForEach(i => Console.WriteLine($"{i.Key} -> {i.Value}"));
         }
     }
--- RandomStuff/Program.cs
+++ RandomStuff/Program.cs
@@ -1,13 +1,23 @@
-﻿namespace RandomStuff
+﻿using System;
+using RandomStuff.MD5;
+namespace RandomStuff
 {
-    using RandomStuff.MD5;
+    class CodeCleanTest
+    {
+        public int Id { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             MD5Gen md5Gen = new MD5Gen();
-            Application app = new Application(md5Gen);
+            RandomNumbersMD5 app = new RandomNumbersMD5(md5Gen);
             app.run();
+
+            CodeCleanTest codeClean = new CodeCleanTest();
+            codeClean.Id = 1;
+            Console.WriteLine(codeClean.Id.ToString());
         }
     }
 }
