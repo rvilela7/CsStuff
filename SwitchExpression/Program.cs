ae8aa9e21979d33a0abcff2968760760dfa32e5e
rvilela7 <ruivil@gmail.com>
2023-08-18 15:14:36 +0100

    SwitchExpression Dem.

---
+++ SwitchExpression/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SwitchExpression
+{
+    public class Program
+    {
+        public enum Operator
+        {
+            ADD,
+            SUBTRACT,
+            MULTIPLY,
+            DIVIDE
+        }
+
+        public class Calculator
+        {
+            public string GetOperatorText(Operator op) => op switch
+                {
+                    Operator.ADD => "Add",
+                    Operator.SUBTRACT => "Subtract",
+                    Operator.MULTIPLY => "Multiply",
+                    Operator.DIVIDE => "Divide",
+                    _ => "No text.",
+                };
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(new Calculator().GetOperatorText(Operator.ADD));
+        }
+    }
+}
